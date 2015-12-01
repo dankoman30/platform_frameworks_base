9e911642e063cf8bc52234ed699590e88bb02440
Roman Birg <roman@cyngn.com>
2015-11-30 23:11:10 -0800

    SystemUI: various qs tile detail improvements
    - Handle top padding inside QSDragPanel, so the detail clipping animation
    doesn't get cut off by the top padding.
    
    - Increase bottom padding of the QSContainer to give some more
    breathing room for the circle page indicator.
    
    - Disable pager overscrolling
    
    - Add dual tile carrot indicators to cast, dnd, and mobile data tiles
    
    Change-Id: I81f56760140a2d7dd31e25b23f477eb1be0af305
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
@@ -134,6 +134,11 @@ public class DndTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
+    protected void handleSecondaryClick() {
+        handleClick();
+    }
+
+    @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         final int zen = arg instanceof Integer ? (Integer) arg : mController.getZen();
         final boolean newValue = zen != Global.ZEN_MODE_OFF;
@@ -199,6 +204,11 @@ public class DndTile extends QSTile<QSTile.BooleanState> {
             mController.removeCallback(mZenCallback);
             Prefs.unregisterListener(mContext, mPrefListener);
         }
+    }
+
+    @Override
+    public boolean hasDualTargetsDetails() {
+        return true;
     }
 
     private final OnSharedPreferenceChangeListener mPrefListener
