4d5f34cd717d121c2b8e1d6e19da96407a52a761
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-12-12 00:30:55 +0100

    Add QS utility classes

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6554,6 +6554,12 @@ public final class Settings {
         public static final String ADB_PORT = "adb_port";
 
         /**
+         * List of QS tile names
+         * @hide
+         */
+        public static final String QS_TILES = "sysui_qs_tiles";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
